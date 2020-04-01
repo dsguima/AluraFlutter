5a5655c03b8671032bc9a91652af333d92640bf6
daniele guimarães <daniele.guimaraes@solutis.com.br>
2020-04-01 14:01:07 -0300

    commit inicial curso 3

---
+++ bytebank3/lib/screens/contatos/lista.dart
@@ -0,0 +1,102 @@
+import 'package:bytebank3/database/dao/contato_dao.dart';
+import 'package:bytebank3/models/contato_bean.dart';
+import 'package:bytebank3/screens/contatos/cadastro.dart';
+import 'package:flutter/material.dart';
+
+class ListaContacts extends StatefulWidget {
+  @override
+  _ListaContactsState createState() => _ListaContactsState();
+}
+
+class _ListaContactsState extends State<ListaContacts> {
+
+  final ContatoDao _dao = ContatoDao();
+
+  @override
+  Widget build(BuildContext context) {
+    //Contacts.add(Contact('daniele', 12134, id:0));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: _listaContatos(),
+      //_list(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _cadastrarContact(context),
+      ),
+    );
+  }
+
+  FutureBuilder<List<Contact>> _listaContatos() {
+    return FutureBuilder(
+      initialData: List(),
+      future: Future.delayed(Duration(seconds: 1)).then((value) => _dao.findAll()),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            break;
+          case ConnectionState.waiting:
+            return _loading();
+            break;
+          case ConnectionState.active:
+            // usar para stream
+            break;
+          case ConnectionState.done:
+            final List<Contact> contacts = snapshot.data;
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                final Contact contact = contacts[index];
+                return ItemContact(contact);
+              },
+              itemCount: contacts.length,
+            );
+            break;
+        }
+        return Text('Erro desconhecido');
+      },
+    );
+  }
+
+  Center _loading() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          CircularProgressIndicator(),
+          SizedBox(height: 20.0,),
+          Text('Loading...'),
+        ],
+      ),
+    );
+  }
+
+  _cadastrarContact(context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => CadastroContact()));
+  }
+}
+
+class ItemContact extends StatelessWidget {
+  final Contact contact;
+
+  ItemContact(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(contact.name,
+          style: TextStyle(
+            fontSize: 24.0,
+          ),),
+        subtitle: Text(contact.accountNumber.toString(),
+          style: TextStyle(
+            fontSize: 16.0,
+          ),),
+      ),
+    );
+  }
+}
+
---
+++ bytebank3/lib/screens/dashboard.dart
@@ -0,0 +1,58 @@
+import 'package:bytebank3/screens/contatos/lista.dart';
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //print(save(Contact('Maria', 1234)));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+      ),
+      body: _body(context),
+    );
+  }
+
+  _body(context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        //Image.network('');
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset('images/bytebank_logo.png'),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Material(
+            color: Theme.of(context).primaryColor,
+            child: InkWell(
+              onTap: () => _navigatorListContacts(context),
+              child: Container(
+                padding: EdgeInsets.all(8.0),
+                width: 150,
+                height: 100,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(Icons.people, color: Colors.white, size: 32.0,),
+                    Text('Contacts', style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  _navigatorListContacts(context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => ListaContacts()));
+  }
+}
